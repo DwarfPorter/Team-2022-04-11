38f30999e23c41ac00405091b7f9ebc40a24c74b
DwarfPorter <mrvlmor@gmail.com>
2022-04-11 21:40:26 +0300

    Кнопка

--- app/src/main/java/ru/geekbrains/team_2022_04_11/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/team_2022_04_11/MainActivity.kt
@@ -2,10 +2,18 @@ package ru.geekbrains.team_2022_04_11
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener {
+            Toast.makeText(this, "Message!", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
